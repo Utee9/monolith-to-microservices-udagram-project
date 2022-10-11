d67b086a9e4284baa3c2c8df8b1c310d79a1a022
Utee <utemma9@gmail.com>
2022-10-11 07:02:14 +0100

    Initial Commit

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_PASSWORD=rinma1699
+export POSTGRES_HOST=udagram-instance.cxlsltybw8wd.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=udagram-350828505179-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
